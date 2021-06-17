d4144e12dc6bc8f50cc349660782941a742bc09a
SOMASEKHAR <77001358+SomuTech@users.noreply.github.com>
2021-06-17 04:29:41 +0000

    5445

--- 7b.c
+++ 7b.c
@@ -14,9 +14,3 @@ int main(){
     printf("\nAfter swap: num1=%d num2=%d",*p1,*p2);  
 return 0;  
 }  
-
-//output:
-enter number1:45                                                                                                                                
-enter number2:32                                                                                                                                
-Before swap:num1=45 num2=32                                                                                                                     
-After swap: num1=32 num2=45
